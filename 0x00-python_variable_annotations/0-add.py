f29beda6bec4d78c33955b23211f4fb0cd2affca
Tsegaye16 <abewatsegaye16@gmail.com>
2024-01-11 20:26:16 +0000

    first day of python back-end

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+defining the type-annotated function that takes two floating number
+as arguments and returns their sum as a float.
+"""
+
+
+def add(a: float, b: floeat) -> floeat:
+    # adding two number and return them
+    return a + b
