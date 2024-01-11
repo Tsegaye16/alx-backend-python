ab5d2f3d48d3d92b3dafe36d84051de97524cc36
Tsegaye16 <abewatsegaye16@gmail.com>
2024-01-11 20:39:08 +0000

    first day of python back-end

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+a type-annotated function which takes a float number as argument
+and returns the floor of the float.
+"""
+
+import math
+
+def floor(n: float) -> int:
+    """convert to int and return them"""
+    return math.floor(n)
