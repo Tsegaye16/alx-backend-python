0a226d98d1b623af7b80bf6683f965e2fcb3408c
Tsegaye16 <abewatsegaye16@gmail.com>
2024-01-11 20:33:04 +0000

    first day of python back-end

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+a type-annotated function that takes two string as arguments
+and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concatnating two string and return them"""
+    return str1 + str2
