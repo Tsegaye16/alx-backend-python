bb0c2607b6bedd140d3bb7d6309100b92e13c895
Tsegaye16 <abewatsegaye16@gmail.com>
2024-01-11 20:54:51 +0000

    first day of python back-end

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-"""Defining and annotating the variables with the specified values"""
+"""Contains definition of variables with specified values and annotations."""
 
 a: int = 1
 pi: float = 3.14
-i_understand_annotations:bool = True
+i_understand_annotations: bool = True
 school: str = "Holberton"
